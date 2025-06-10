bea6aac75b3a89273c63b8854514e13661e85ce0
xhinntzy <voc27297@gmail.com>
2025-06-10 11:15:13 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -7,11 +7,11 @@ MAX_BOT = int(os.getenv("MAX_BOT", "60"))
 
 DEVS = list(map(int, os.getenv("DEVS", "7853215553").split()))
 
-API_ID = int(os.getenv("API_ID", "25546347"))
+API_ID = int(os.getenv("API_ID", "14723727"))
 
-API_HASH = os.getenv("API_HASH", "ffd96679fb5f663c7b2b67addf8daadb")
+API_HASH = os.getenv("API_HASH", "e07ba1df9c9729f6fa40aa605acb280e")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7564096959:AAHe_hGMNkx-Z8aTAK2xpPD7VUSmr7IbRk8")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8158032880:AAEdnK5QkCZrVg5VQh0vPMv8S2NsZFpzhQs")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "7853215553"))
 
@@ -19,6 +19,6 @@ BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002
 
 RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://ml9079716:48gxGsQhTHWumLI15@cluster0.6e1q9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://briandinda1235:vMgM8VWt33zvDkX@cluster0.u2pldih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002331639417"))
